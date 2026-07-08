8b2def2e0d04ebbc6e2bbe91cf2499c2bf90610f
southen_ <alabanaomi2020@icloud.com>
2026-07-08 15:25:41 +0100

    Fix Privy Solana Mock issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rolldownOptions: {
-      external: [
-        '@solana/kit',
-        '@solana-program/system',
-        '@solana-program/token'
-      ]
+  resolve: {
+    alias: {
+      '@solana/kit': path.resolve(__dirname, './src/mocks/solana.ts'),
+      '@solana-program/system': path.resolve(__dirname, './src/mocks/solana.ts'),
+      '@solana-program/token': path.resolve(__dirname, './src/mocks/solana.ts'),
+      '@solana-program/token-2022': path.resolve(__dirname, './src/mocks/solana.ts')
     }
   }
 })
